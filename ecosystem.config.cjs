d734ae77d22ceb609d2ccbcc5370ab244b38c3ca
RonakDarji1997 <ronakdarji1997@gmail.com>
2026-01-09 17:07:39 -0500

    Change backend port to 6002

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -12,7 +12,7 @@ module.exports = {
       max_memory_restart: '1G',
       env: {
         NODE_ENV: 'development',
-        PORT: 5000,
+        PORT: 6002,
         VITE_PORT: 6001
       },
       error_file: './logs/dev-error.log',
